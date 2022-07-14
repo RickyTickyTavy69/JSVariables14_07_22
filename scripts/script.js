7a89f18daa7f36e3eccf393f13e8fbd9d92d0865
artem <bakirovartem69@gmail.com>
2022-07-14 15:12:23 +0200

    first one

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+/*var z = 34;
+
+z = 67;
+
+console.log("z:", z);*/
+
+/*let x = 34;
+x = 67;
+console.log("x:", x);*/
+
+/*const y = 34;
+y = 67;
+console.log("y:", y);*/
+
+const carName = "BMW";
+console.log(carName);
+const x = 150;
+console.log(x);
+const y = 50;
+console.log(y);
+const z = x + y;
+console.log(z);
+
+let firstName = "John",
+  lastName = "Doe",
+  age = "35";
+
+console.log("firstName:", firstName, "lastName:", lastName, "age:", age);
